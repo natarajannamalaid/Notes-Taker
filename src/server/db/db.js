7def81527f591dd3bc460845af612a0798a6fefa
natarajannamalaid <nataraj.annamalaid@gmail.com>
2016-08-30 14:09:32 -0400

    Notes-Taker
    This is a MEAN Stack Application done with Web Pack

---
+++ src/server/db/db.js
@@ -0,0 +1,8 @@
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/todos');
+var Todo=mongoose.model('Todo',{
+	task: String,
+	isCompleted: Boolean,
+	isEditing: Boolean
+});
+module.exports.Todo=Todo;
